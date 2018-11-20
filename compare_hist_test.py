d1135e996a849e963849ee03b573e804621428db
wivj4zm4fpg0 <nzswlrkad8cw@gmail.com>
2018-11-20 13:39:01 +0900

    first commit

---
+++ compare_hist_test.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy
+
+
+def compare_images(image1: numpy.ndarray, image2: numpy.ndarray):
+    similarity_value = 0
+    for i in range(3):
+        target_hist = cv2.calcHist([image1], [i], None, [256], [0, 256])
+        comparing_hist = cv2.calcHist([image2], [i], None, [256], [0, 256])
+        similarity_value += cv2.compareHist(target_hist, comparing_hist, 0)
+
+    return similarity_value / 3
+
+
+if __name__ == '__main__':
+    value = compare_images(cv2.imread('Histogram_Comparison_Source_0.jpg'),
+                           cv2.imread('Histogram_Comparison_Source_1.jpg'))
+
+    print(value)
